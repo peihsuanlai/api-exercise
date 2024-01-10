4898e37d806a9f547724d6854b1022d1deaaef61
peihsuan <june625678@gmail.com>
2024-01-10 23:33:56 +0800

    完成兩個頁面

---
+++ login.js
@@ -0,0 +1,34 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+const app = {
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      url: "https://vue3-course-api.hexschool.io/v2",
+      path: "product-api",
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(`${this.url}/admin/signin`, this.user)
+        .then((res) => {
+          console.log(res.data);
+          const { token, expired } = res.data;
+          console.log(token);
+          document.cookie = `myToken=${token}; expires=${new Date(expired)};`;
+          location.href = "/products.html";
+        })
+        .catch((err) => {
+          alert(err.data.message);
+          this.user.username = "";
+          this.user.password = "";
+        });
+    },
+  },
+};
+
+createApp(app).mount("#app");
---
+++ products.js
@@ -0,0 +1,57 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+const app = {
+  data() {
+    return {
+      products: [],
+      temp: {},
+      url: "https://vue3-course-api.hexschool.io/v2",
+      path: "product-api",
+      length: 0,
+    };
+  },
+  methods: {
+    getProducts() {
+      axios
+        .get(`${this.url}/api/${this.path}/admin/products`)
+        .then((res) => {
+          this.products = res.data.products;
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        });
+    },
+    showProduct(id) {
+      axios
+        .get(`${this.url}/api/${this.path}/product/${id}`)
+        .then((res) => {
+          this.temp = res.data.product;
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        });
+    },
+    check() {
+      axios
+        .post(`${this.url}/api/user/check`)
+        .then(() => {
+          this.getProducts();
+        })
+        .catch((err) => {
+          alert(err.data.message);
+          location.href = "/login.html";
+        });
+    },
+  },
+  mounted() {
+    const token = document.cookie.replace(
+      /(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    //console.log(token);
+    axios.defaults.headers.common["Authorization"] = token;
+    this.check();
+  },
+};
+
+createApp(app).mount("#app");
