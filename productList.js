d455b3627b240df8af66224db39232f744f9c02f
peihsuan <june625678@gmail.com>
2024-02-03 15:35:51 +0800

    第四周作業

--- productList.js
+++ productList.js
@@ -1,130 +1,56 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
-
-const app = {
+let productModal = null;
+let deleteModal = null;
+let url = "https://vue3-course-api.hexschool.io/v2";
+let path = "product-api";
+const app = createApp({
   data() {
     return {
-      products: [],
-      tempProduct: {},
+      products: [], 
+      tempProduct: {
+        imagesUrl: [],
+      },
       url: "https://vue3-course-api.hexschool.io/v2",
       path: "product-api",
       isNew: null,
-      modal1: {},
-      modal2: {},
+      pagination:{}
     };
   },
   methods: {
-    getProducts() {
+    getProducts(page=1) {
       axios
-        .get(`${this.url}/api/${this.path}/admin/products`)
+        .get(`${url}/api/${path}/admin/products?page=${page}`)
         .then((res) => {
-          this.products = res.data.products;
+          //console.log(res);
+          const {products, pagination}=res.data;
+          this.products = products;
+          this.pagination = pagination;
         })
         .catch((err) => {
           alert(err.data.message);
+          location.href = "index.html";
         });
     },
-    openModal(modalName, isNew, item) {
-      if (modalName == "productModal") {
-        if (isNew) {
-          this.tempProduct = {};
-        } else {
-          this.tempProduct = { ...item };
-        }
-        this.isNew = isNew;
-        this.modal1.show();
-      } else if (modalName == "deleteModal") {
+    openModal(isNew, item) {
+      if (isNew == "new") {
+        this.tempProduct = {
+          imagesUrl: [],
+        };
+        this.isNew = true;
+        productModal.show();
+      } else if (isNew == "edit") {
         this.tempProduct = { ...item };
-        this.modal2.show();
+        this.isNew = false;
+        productModal.show();
+      } else if (isNew == "delete") {
+        this.tempProduct = { ...item };
+        deleteModal.show();
       }
     },
-    hideModal(modalName) {
-      if (modalName == "productModal") {
-        this.modal1.hide();
-      } else if (modalName == "deleteModal") {
-        this.modal2.hide();
-      }
-    },
-    updateData(item) {
-      this.tempProduct = item;
-      console.log(this.isNew);
-      let url = `${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`;
-      let http = "put";
-      if (this.isNew) {
-        url = `${this.url}/api/${this.path}/admin/product`;
-        http = "post";
-      }
-      axios[http](url, { data: this.tempProduct })
-        .then((res) => {
-          alert(res.data.message);
-          this.hideModal("productModal");
-          this.getProducts();
-        })
-        .catch((err) => {
-          alert(err.data.message);
-        });
 
-      // if (this.isNew) {
-      //   axios
-      //     .post(`${this.url}/api/${this.path}/admin/product`, this.tempProduct)
-      //     .then((res) => {
-      //       console.log(res);
-      //       this.hideModal("productModal");
-      //       this.getProducts();
-
-      //     })
-      //     .catch((err) => {
-      //       console.log(err);
-      //       alert(err.data.message);
-      //     });
-      // } else {
-      //   axios
-      //     .put(
-      //       `${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`,
-      //       this.tempProduct
-      //     )
-      //     .then((res) => {
-      //       console.log(res);
-      //       this.hideModal("productModal");
-      //       this.getProducts();
-
-      //     })
-      //     .catch((err) => {
-      //       console.log(err);
-      //       alert(err.data);
-      //     });
-      // }
-    },
-    deleteData(){
-      axios
-        .delete(`${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`)
-        .then((res) => {
-          alert(res.data.message);
-          this.hideModal("deleteModal");
-          this.getProducts();
-        })
-        .catch((err) => {
-          alert(err.data.message);
-        });
-    },
-    uploadFile() {
-      const uploadedFile = this.$refs.fileInput.files[0];
-      const formData = new FormData();
-      formData.append("file-to-upload", uploadedFile);
-      axios
-        .post(`${this.url}/api/${this.path}/admin/upload`,formData)
-        .then((res) => {
-          this.tempProduct.imageUrl = res.data.imageUrl;
-        })
-        .catch((err) => {
-          alert(err.data.message);
-        });
-    },
-    createImages(){
-      this.tempProduct.imagesUrl=[];
-    },
     check() {
       axios
-        .post(`${this.url}/api/user/check`)
+        .post(`${url}/api/user/check`)
         .then(() => {
           this.getProducts();
         })
@@ -141,8 +67,85 @@ const app = {
     );
     axios.defaults.headers.common["Authorization"] = token;
     this.check();
-    this.modal1 = new bootstrap.Modal(this.$refs.productModal);
-    this.modal2 = new bootstrap.Modal(this.$refs.delProductModal);
   },
-};
-createApp(app).mount("#app");
+});
+
+app.component("pagination",{
+  props:["pages"],
+  template:"#pagination",
+  methods:{
+    switchPage(page){
+      this.$emit("switchPage",page);
+    }
+  }
+})
+
+app.component("productModal", {
+  props: ["product", "isNew"],
+  template: "#productModal",
+  methods: {
+    updateData() {
+      let apiUrl = `${url}/api/${path}/admin/product/${this.product.id}`;
+      let http = "put";
+      if (this.isNew) {
+        apiUrl = `${url}/api/${path}/admin/product`;
+        http = "post";
+      }
+      axios[http](apiUrl, { data: this.product })
+        .then((res) => {
+          //console.log(res.config.data);
+          alert(res.data.message);
+          productModal.hide();
+          this.$emit("update");
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        });
+    },
+    uploadFile() {
+      const uploadedFile = this.$refs.fileInput.files[0];
+      const formData = new FormData();
+      formData.append("file-to-upload", uploadedFile);
+      axios
+        .post(`${url}/api/${path}/admin/upload`, formData)
+        .then((res) => {
+          this.product.imageUrl = res.data.imageUrl;
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        });
+    },
+    createImages() {
+      this.product.imagesUrl = [];
+    },
+  },
+  mounted() {
+    productModal = new bootstrap.Modal(this.$refs.productModal);
+  },
+});
+
+app.component("deleteModal",{
+  props: ["product", "isNew"],
+  template: "#delProductModal",
+  methods:{
+    deleteData() {
+      axios
+        .delete(
+          `${url}/api/${path}/admin/product/${this.product.id}`
+        )
+        .then((res) => {
+          alert(res.data.message);
+          deleteModal.hide();
+          this.$emit("update");
+        })
+        .catch((err) => {
+          alert(err.data.message);
+        });
+    },
+  },
+  mounted(){
+    deleteModal = new bootstrap.Modal(this.$refs.delProductModal);
+  }
+})
+
+app.mount("#app");
